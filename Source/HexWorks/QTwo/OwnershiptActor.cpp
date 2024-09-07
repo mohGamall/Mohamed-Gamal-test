9651756b576b383cfbd7ff397db4e42c55a3f4fe
Mohamed Gamal <moh.gamall@hotmail.com>
2024-09-07 17:06:49 +0300

    Q2 Ownership

---
+++ Source/HexWorks/QTwo/OwnershiptActor.cpp
@@ -0,0 +1,38 @@
+﻿
+
+
+#include "OwnershiptActor.h"
+
+
+// Sets default values
+AOwnershiptActor::AOwnershiptActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AOwnershiptActor::BeginPlay()
+{
+	Super::BeginPlay();
+	// Create a shared instance of B
+	TSharedPtr<B> BSharedPTR = MakeShared<B>();
+	// Create an instance of A, passing a weak pointer to B
+	TSharedPtr<A> ASharedPTR = MakeShared<A>(BSharedPTR);
+	// C also owns A (shared ownership)
+	C c;
+	c.SetA(ASharedPTR);
+	// A accesses information from B
+	ASharedPTR->AccessB(); //should output "Accessing B" and "Printing info from B"
+	// destruction of B
+	BSharedPTR.Reset();
+	// Then A tries to access B again after B has been destroyed
+	ASharedPTR->AccessB(); //so it outputs "B is no longer valid"
+}
+
+// Called every frame
+void AOwnershiptActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/HexWorks/QTwo/OwnershiptActor.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "OwnershiptActor.generated.h"
+
+
+class B
+{
+public:
+	static void PrintInfoFromB()
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Printing Info from B"));
+	}
+};
+
+class A
+{
+public:
+	A(TWeakPtr<B> InB) : BWeakPtr(InB) {}
+
+	void AccessB() const
+	{
+		//lock the weak pointer to access the object if it is still valid
+		if (TSharedPtr<B> BSharedPtr = BWeakPtr.Pin())
+		{
+			//access the object
+			UE_LOG(LogTemp, Warning, TEXT("Accessing B"));
+			BSharedPtr->PrintInfoFromB();
+		} 
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("B is no longer valid"));
+		}
+	}
+
+
+private:
+	TWeakPtr<B> BWeakPtr;
+	
+};
+
+
+class C
+{
+public:
+	// C holds a shared pointer to A, sharing ownership with B
+	void SetA(TSharedPtr<A> InA )
+	{
+		APtr = InA;
+	}
+private:
+	TSharedPtr<A> APtr;
+};
+
+UCLASS()
+class HEXWORKS_API AOwnershiptActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AOwnershiptActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
