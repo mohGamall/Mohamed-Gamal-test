010b5a5d3b7d32d54abf0a1e49d7a134650e864f
Mohamed Gamal <moh.gamall@hotmail.com>
2024-09-07 18:51:43 +0300

    Q4 ForwardingFunction

---
+++ Source/HexWorks/Q4/Q4FunctionRouter.cpp
@@ -0,0 +1,29 @@
+﻿
+
+
+#include "Q4FunctionRouter.h"
+
+
+// Sets default values
+AQ4FunctionRouter::AQ4FunctionRouter()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AQ4FunctionRouter::BeginPlay()
+{
+	Super::BeginPlay();
+	some_type Obj;
+	ForwardingFunction(Obj);// this will call the Lvalue ref version
+	ForwardingFunction(some_type()); // this will call the Rvalue ref version
+	
+}
+
+// Called every frame
+void AQ4FunctionRouter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/HexWorks/Q4/Q4FunctionRouter.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Q4FunctionRouter.generated.h"
+
+class some_type {};
+
+UCLASS()
+class HEXWORKS_API AQ4FunctionRouter : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AQ4FunctionRouter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void func(const some_type &Param){ UE_LOG(LogTemp, Warning, TEXT("called Lvalue ref version")) }
+	void func( some_type &&Param){ UE_LOG(LogTemp, Warning, TEXT("called Rvalue ref version")) }
+
+	//the universal forwarding function
+	template<typename T>
+	void ForwardingFunction(T &&Param)
+	{
+		func(Forward<T>(Param));
+	}
+};
